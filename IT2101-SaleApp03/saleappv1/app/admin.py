bcc2d92798d3af32d28a8a540e5f89d876771dd5
uncle3chicken@gmail.com <uncle3chicken@gmail.com>
2023-11-13 09:42:21 +0700

    hehe

---
+++ IT2101-SaleApp03/saleappv1/app/admin.py
@@ -0,0 +1,23 @@
+from app.models import Category,Product
+from app import app,db
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import BaseView,expose
+class MyProductView(ModelView):
+    column_list = ["id","name","price","image"]
+    column_searchable_list = ["name"]
+    column_filters = ["name","price"]
+    column_editable_list = ["name","price"]
+    edit_modal = True
+class MyCategoryView(ModelView):
+    column_list = ["name","id","products"]
+class StatsView(BaseView):
+    @expose("/")
+    def index(self):
+        return self.render("admin/stats.html")
+
+
+admin = Admin(app,name="Quản trị bán hàng",template_mode="bootstrap4")
+admin.add_view(MyCategoryView(Category,db.session))
+admin.add_view(MyProductView(Product,db.session))
+admin.add_view((StatsView(name="Thống kê báo cáo")))
--- IT2101-SaleApp03/saleappv1/app/models.py
+++ IT2101-SaleApp03/saleappv1/app/models.py
@@ -1,20 +1,35 @@
 from sqlalchemy import Column, Integer, String,Float,Boolean,ForeignKey
 from sqlalchemy.orm import relationship
 from app import db,app
+from flask_login import UserMixin
+class User(db.Model,UserMixin):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=True)
+    account = Column(String(50),unique=True,nullable=True)
+    password= Column(String(50),nullable=True)
 class Category(db.Model):
     __tablename__="category"
     id= Column(Integer, primary_key=True,autoincrement=True)
     name= Column(String(50),nullable=True)
     products = relationship("Product", backref="category", lazy= True)
+    def __str__(self):
+        return self.name
 class Product(db.Model):
     id  = Column(Integer,primary_key=True, autoincrement=True)
     name= Column(String(50),nullable=True)
     price = Column(Float,default=0)
-    image = Column(String(100),default="https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg")
+    image = Column(String(255),default="https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg")
     active = Column(Boolean,default=True)
     category_id= Column(Integer,ForeignKey(Category.id),nullable=False)
+    def __str__(self):
+        return  self.name
 if __name__=="__main__":
     with app.app_context():
+        db.create_all()
+        import hashlib
+        u= User(name="admin",account="admin",password=str(hashlib.md5("123".encode("utf-8")).hexdigest()))
+        db.session.add(u)
+        db.session.commit()
         """c1= Category(name="Mobile")
         c2 = Category(name="Tablet")
         c3 = Category(name="Desktop")
@@ -22,11 +37,11 @@ if __name__=="__main__":
         db.session.add(c2)
         db.session.add(c3)
         db.session.commit()"""
-        db.create_all()
-        p1= Product(name="SamSung galaxy",price= 2000000,category_id= 1)
-        p2 = Product(name="iPhone", price=2000000, category_id=1)
-        p3 = Product(name="SamSung galaxy s23", price=2000000, category_id=1)
-        p4 = Product(name="SamSung galaxy ultra", price=2000000, category_id=1)
-        db.session.add_all([p1,p2,p3,p4])
-        db.session.commit()
+
+        # p1= Product(name="SamSung galaxy",price= 2000000,category_id= 1)
+        # p2 = Product(name="iPhone", price=2000000, category_id=1)
+        # p3 = Product(name="SamSung galaxy s23", price=2000000, category_id=1)
+        # p4 = Product(name="SamSung galaxy ultra", price=2000000, category_id=1)
+        # db.session.add_all([p1,p2,p3,p4])
+        # db.session.commit()
         """ánkjdasdkasbkdbashkdb"""
